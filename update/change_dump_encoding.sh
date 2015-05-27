afb2bb70d377cce1dca2519b39097f6b85628a92
Marek Ramotowski <ma-ra@users.noreply.github.com>
2015-05-27 08:51:39 +0200

    Drobne poprawki w alter.sql i view.sql. Dodanie change_dump_encoding.sh - rozwiązuje problem niezgodności kodowania.

---
+++ update/change_dump_encoding.sh
@@ -0,0 +1,1 @@
+sed -i 's/CHARACTER SET latin2 //g' $1
--- update/views.sql
+++ update/views.sql
@@ -15,44 +15,26 @@ GROUP BY `warehouse`.`article_number`
 ORDER BY `warehouse`.`article_number`;
 
 --
--- Struktura widoku `rap_textile`
+-- Struktura widoku `rap_shopping`
 --
 CREATE OR REPLACE
  ALGORITHM = UNDEFINED
  SQL SECURITY INVOKER
- VIEW `rap_textile`
+ VIEW `rap_shopping`
  AS SELECT
-`order`.`textil_pair` AS `textil_pair`,
-`supplier1`.`supplier_name` AS `supplier1_name`,
-`textile1`.`textile_number` AS `supplier1_number`,
-`supplier2`.`supplier_name` AS `supplier2_name`,
-`textile2`.`textile_number` AS `supplier2_number`,
+`textile`.`textile_number`,
+SUM(`article_amount`) as article_amount_sum,
+SUM(`article_calculated_amount`) as article_calculated_amount_sum
 
--- textile1_selected
-SUM(IF(`order`.`textil_pair`,(`article`.`article_first_textile_amount` * `order`.`article_amount`),(`article`.`article_all_textile_amount` * `order`.`article_amount`))) AS `textile1_selected`,
--- textile2_selected
-SUM(IF(`order`.`textil_pair`,(`article`.`article_second_textile_amount` * `order`.`article_amount`),NULL)) AS `textile2_selected`,
--- to check
-group_concat(concat(' ',cast(`order`.`article_amount` as char charset utf8mb4),'x ',convert(`order`.`order_number` using utf8mb4)) separator ',') AS `order_number`,
-group_concat(concat(' (',`textile1`.`textile_name`,ifnull(concat(' ',`textile2`.`textile_name`,')'),')')) separator ',') AS `order_reference`
+FROM `shopping`
+LEFT JOIN `textile`
+   ON `shopping`.`textile_textile_id` = `textile`.`textile_id`
+LEFT JOIN `warehouse`
+   ON `shopping`.`shopping_id` = `warehouse`.`shopping_shopping_id`
 
-FROM `order`
-LEFT JOIN `article`
-    ON `order`.`article_article_id` = `article`.`article_id`
-LEFT JOIN `textile` `textile1`
-    ON `order`.`textile1_textile_id` = `textile1`.`textile_id`
-LEFT JOIN `supplier` `supplier1`
-    ON `textile1`.`supplier_supplier_id` = `supplier1`.`supplier_id`
-LEFT JOIN `textile` `textile2`
-    ON `order`.`textile2_textile_id` = `textile2`.`textile_id`
-LEFT JOIN `supplier` `supplier2`
-    ON `textile2`.`supplier_supplier_id` = `supplier2`.`supplier_id`
-LEFT JOIN `rap_warehouse`
-    ON `rap_warehouse`.`article_number` = `textile1`.`textile_number`
-    
-WHERE `order`.`checked` =1
-GROUP BY `order`.`textil_pair`,`supplier1`.`supplier_name`,`textile1`.`textile_number`,`supplier2`.`supplier_name`,`textile2`.`textile_number`
-ORDER BY `supplier1`.`supplier_name`,`supplier2`.`supplier_name`,`textile1`.`textile_number`,`textile2`.`textile_number`
+WHERE `warehouse`.`article_count` IS NULL
+GROUP BY `textile`.`textile_number`
+ORDER BY `textile`.`textile_number` ASC;
 
 --
 -- Struktura widoku `rap_textile2`
@@ -108,7 +90,7 @@ LEFT JOIN `article` `article1`
 LEFT JOIN `order` `order2`
     ON `textile`.`textile_id` = `order2`.`textile2_textile_id`
 LEFT JOIN `article` `article2`
-    ON `order2`.`article_article_id` = `article2`.`article_id`
+    ON `order2`.`article_article_id` = `article2`.`article_id`;
 
 --
 -- Struktura widoku `rap_textile2-1`
@@ -129,26 +111,46 @@ SUM(`rap_textile2`.`textiles_selected`) - `rap_textile2`.`textile1_warehouse` - 
 FROM `rap_textile2`
 WHERE `order1_checked` = 1 OR `order2_checked` = 1
 GROUP BY `rap_textile2`.`supplier_name`,`rap_textile2`.`textile_number`,`rap_textile2`.`textile1_warehouse`,`rap_textile2`.`textiles_ordered`
-ORDER BY `rap_textile2`.`textile_number`
+ORDER BY `rap_textile2`.`textile_number`;
 
 --
--- Struktura widoku `rap_textile2-1`
+-- Struktura widoku `rap_textile`
 --
 CREATE OR REPLACE
  ALGORITHM = UNDEFINED
  SQL SECURITY INVOKER
- VIEW `rap_shopping`
+ VIEW `rap_textile`
  AS SELECT
-`textile`.`textile_number`,
-SUM(`article_amount`) as article_amount_sum,
-SUM(`article_calculated_amount`) as article_calculated_amount_sum
+`order`.`textil_pair` AS `textil_pair`,
+`supplier1`.`supplier_name` AS `supplier1_name`,
+`textile1`.`textile_number` AS `supplier1_number`,
+`supplier2`.`supplier_name` AS `supplier2_name`,
+`textile2`.`textile_number` AS `supplier2_number`,
 
-FROM `shopping`
-LEFT JOIN `textile`
-   ON `shopping`.`textile_textile_id` = `textile`.`textile_id`
-LEFT JOIN `warehouse`
-   ON `shopping`.`shopping_id` = `warehouse`.`shopping_shopping_id`
+-- textile1_selected
+SUM(IF(`order`.`textil_pair`,(`article`.`article_first_textile_amount` * `order`.`article_amount`),(`article`.`article_all_textile_amount` * `order`.`article_amount`))) AS `textile1_selected`,
+-- textile2_selected
+SUM(IF(`order`.`textil_pair`,(`article`.`article_second_textile_amount` * `order`.`article_amount`),NULL)) AS `textile2_selected`,
+-- to check
+group_concat(concat(' ',cast(`order`.`article_amount` as char charset utf8mb4),'x ',convert(`order`.`order_number` using utf8mb4)) separator ',') AS `order_number`,
+group_concat(concat(' (',`textile1`.`textile_name`,ifnull(concat(' ',`textile2`.`textile_name`,')'),')')) separator ',') AS `order_reference`
 
-WHERE `warehouse`.`article_count` IS NULL
-GROUP BY `textile`.`textile_number`
-ORDER BY `textile`.`textile_number` ASC
+FROM `order`
+LEFT JOIN `article`
+    ON `order`.`article_article_id` = `article`.`article_id`
+LEFT JOIN `textile` `textile1`
+    ON `order`.`textile1_textile_id` = `textile1`.`textile_id`
+LEFT JOIN `supplier` `supplier1`
+    ON `textile1`.`supplier_supplier_id` = `supplier1`.`supplier_id`
+LEFT JOIN `textile` `textile2`
+    ON `order`.`textile2_textile_id` = `textile2`.`textile_id`
+LEFT JOIN `supplier` `supplier2`
+    ON `textile2`.`supplier_supplier_id` = `supplier2`.`supplier_id`
+LEFT JOIN `rap_warehouse`
+    ON `rap_warehouse`.`article_number` = `textile1`.`textile_number`
+    
+WHERE `order`.`checked` =1
+GROUP BY `order`.`textil_pair`,`supplier1`.`supplier_name`,`textile1`.`textile_number`,`supplier2`.`supplier_name`,`textile2`.`textile_number`
+ORDER BY `supplier1`.`supplier_name`,`supplier2`.`supplier_name`,`textile1`.`textile_number`,`textile2`.`textile_number`;
+
+
