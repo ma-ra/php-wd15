96ad75f66414790c2b2a48e13ecbc032a817d4aa
Marek Ramotowski <ma-ra@users.noreply.github.com>
2016-10-03 11:36:00 +0200

    Poprawiono widok rap_textiles. Dzięki temu zużycie materiałow będzie się dobrze liczyć, a modele bez określonego zużycia będą traktowane jako zużycie na poziomie 1mln.

--- dokumentacja/views.sql
+++ dokumentacja/views.sql
@@ -29,18 +29,14 @@ CREATE OR REPLACE
 `order1`.`order_id` AS `order1_id`,
 `order1`.`order_number` AS `order1_number`,
 `order1`.`checked` AS `order1_checked`,
-`order2`.`order_id` AS `order2_id`,
-`order2`.`order_number` AS `order2_number`,
-`order2`.`checked` AS `order2_checked`,
+NULL AS `order2_id`,
+NULL AS `order2_number`,
+NULL AS `order2_checked`,
 
--- @textile1_selected := IF(`order1`.`textil_pair`,`article1`.`article_first_textile_amount` * `order1`.`article_amount`,`article1`.`article_all_textile_amount` * `order1`.`article_amount`) AS `textile1_selected`,
 IF(`order1`.`textil_pair`,`article1`.`article_first_textile_amount` * `order1`.`article_amount`,`article1`.`article_all_textile_amount` * `order1`.`article_amount`) AS `textile1_selected`,
--- @textile2_selected := `article2`.`article_second_textile_amount` * `order2`.`article_amount` AS `textile2_selected`,
-`article2`.`article_second_textile_amount` * `order2`.`article_amount` AS `textile2_selected`,
--- @textiles_selected := IFNULL(@textile1_selected,0) + IFNULL(@textile2_selected,0) AS `textiles_selected`,
-IFNULL(IF(`order1`.`textil_pair`,`article1`.`article_first_textile_amount` * `order1`.`article_amount`,`article1`.`article_all_textile_amount` * `order1`.`article_amount`),0)
-    + IFNULL(`article2`.`article_second_textile_amount` * `order2`.`article_amount`,0)
-    AS `textiles_selected`,
+NULL AS `textile2_selected`,
+IFNULL(IF(`order1`.`textil_pair`,`article1`.`article_first_textile_amount` * `order1`.`article_amount`,`article1`.`article_all_textile_amount` * `order1`.`article_amount`),1000000) AS `textiles_selected`,
+
 IF(`rap_shopping`.`article_ordered`=0,NULL, `rap_shopping`.`article_ordered`) as textiles_ordered
 
 FROM`textile`
@@ -57,6 +53,40 @@ LEFT JOIN `order` `order1`
     ON `textile`.`textile_id` = `order1`.`textile1_textile_id`
 LEFT JOIN `article` `article1`
     ON `order1`.`article_article_id` = `article1`.`article_id`
+
+LEFT JOIN `order` `order2`
+    ON `textile`.`textile_id` = `order2`.`textile2_textile_id`
+LEFT JOIN `article` `article2`
+    ON `order2`.`article_article_id` = `article2`.`article_id`
+	
+UNION ALL
+
+SELECT
+`supplier`.`supplier_name` AS `supplier_name`,
+`textile`.`textile_number` AS `textile_number`,
+`fabric_collection`.`fabric_name` AS `fabric_name`,
+NULL AS `order1_id`,
+NULL AS `order1_number`,
+NULL AS `order1_checked`,
+`order2`.`order_id` AS `order2_id`,
+`order2`.`order_number` AS `order2_number`,
+`order2`.`checked` AS `order2_checked`,
+
+NULL AS `textile1_selected`,
+`article2`.`article_second_textile_amount` * `order2`.`article_amount` AS `textile2_selected`,
+IFNULL(`article2`.`article_second_textile_amount` * `order2`.`article_amount`,1000000) AS `textiles_selected`,
+	
+IF(`rap_shopping`.`article_ordered`=0,NULL, `rap_shopping`.`article_ordered`) as textiles_ordered
+
+FROM`textile`
+
+LEFT JOIN `fabric_collection`
+    ON `textile`.`textile_number` = `fabric_collection`.`fabric_number`
+LEFT JOIN `supplier`
+    ON `fabric_collection`.`supplier_supplier_id` = `supplier`.`supplier_id`
+    
+LEFT JOIN `rap_shopping`
+    ON `fabric_collection`.`fabric_number` = `rap_shopping`.`fabric_number`
 
 LEFT JOIN `order` `order2`
     ON `textile`.`textile_id` = `order2`.`textile2_textile_id`
